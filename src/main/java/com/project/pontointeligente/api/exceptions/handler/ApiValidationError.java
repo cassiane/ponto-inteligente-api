618f85deb65d733514074ee8ed93102947eacad0
Anderson L. Santos <leal.ba@gmail.com>
2018-05-23 19:40:24 -0300

    Alterando tratamento global de exceções para lidar com exceções geradas para o javax.validation

--- src/main/java/com/project/pontointeligente/api/exceptions/handler/ApiValidationError.java
+++ src/main/java/com/project/pontointeligente/api/exceptions/handler/ApiValidationError.java
@@ -2,21 +2,35 @@ package com.project.pontointeligente.api.exceptions.handler;
 
 import org.springframework.http.HttpStatus;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class ApiValidationError extends ApiError {
 
-    private String field;
+    private List<ApiValidationDetail> details;
 
-    ApiValidationError(HttpStatus status, String field, Throwable ex) {
-        super(status, ex);
-        this.field = field;
-    }
-
-    ApiValidationError(HttpStatus status, String field, String message, Throwable ex) {
+    ApiValidationError(HttpStatus status, String message, Throwable ex) {
         super(status, message, ex);
-        this.field = field;
+        this.details = new ArrayList<>();
     }
 
-    public String getField() {
-        return field;
+    ApiValidationError(HttpStatus status, String message, String field, String detailMessage, Throwable ex) {
+        super(status, message, ex);
+        this.details = new ArrayList<>();
+        addDetail(field, detailMessage);
     }
+
+    ApiValidationError(HttpStatus status, String message, List<ApiValidationDetail> details, Throwable ex) {
+        super(status, message, ex);
+        this.details = details;
+    }
+
+    void addDetail(String field, String detailMessage) {
+        this.details.add(new ApiValidationDetail(field, detailMessage));
+    }
+
+    public List<ApiValidationDetail> getDetails() {
+        return details;
+    }
+
 }
--- src/main/java/com/project/pontointeligente/api/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/com/project/pontointeligente/api/exceptions/handler/GlobalExceptionHandler.java
@@ -6,16 +6,43 @@ import com.project.pontointeligente.api.exceptions.ResourceNotFoundException;
 import com.project.pontointeligente.api.exceptions.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final String MESSAGE_ENTRADA_INVALIDA = "Entrada inválida";
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiValidationError> handle(MethodArgumentNotValidException e) {
+        HttpStatus httpStatus = HttpStatus.UNPROCESSABLE_ENTITY;
+        List<ApiValidationDetail> details = toApiValidationDetailList(e);
+        ApiValidationError apiError = new ApiValidationError(httpStatus, MESSAGE_ENTRADA_INVALIDA, details, e);
+        return new ResponseEntity<>(apiError, httpStatus);
+    }
+
+    private List<ApiValidationDetail> toApiValidationDetailList(MethodArgumentNotValidException e) {
+        BeanPropertyBindingResult bindingResult = (BeanPropertyBindingResult) e.getBindingResult();
+        return bindingResult.getAllErrors()
+                            .stream()
+                            .map(error -> (FieldError) error)
+                            .map(error -> new ApiValidationDetail(error.getField(), error.getDefaultMessage()))
+                            .collect(toList());
+    }
+
     @ExceptionHandler(value = ValidationException.class)
     public ResponseEntity<ApiValidationError> handle(ValidationException e) {
         HttpStatus httpStatus = HttpStatus.UNPROCESSABLE_ENTITY;
-        ApiValidationError apiError = new ApiValidationError(httpStatus, e.getField(), e.getMessage(), e);
+        ApiValidationError apiError = new ApiValidationError(httpStatus, MESSAGE_ENTRADA_INVALIDA, e.getField(), e.getMessage(), e);
         return new ResponseEntity<>(apiError, httpStatus);
     }
 
